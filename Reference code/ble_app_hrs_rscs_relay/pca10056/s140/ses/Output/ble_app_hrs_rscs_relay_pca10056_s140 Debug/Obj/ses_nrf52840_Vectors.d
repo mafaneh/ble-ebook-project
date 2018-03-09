9512d77180b35435a84a412b0e4223704ea2246e
Mohammad Afaneh <mafaneh@users.noreply.github.com>
2018-03-09 11:45:55 -0500

    Removed unnecessary files.

--- Reference code/ble_app_hrs_rscs_relay/pca10056/s140/ses/Output/ble_app_hrs_rscs_relay_pca10056_s140 Debug/Obj/ses_nrf52840_Vectors.d
+++
@@ -1,2 +0,0 @@
-Output/ble_app_hrs_rscs_relay_pca10056_s140\ Debug/Obj/ses_nrf52840_Vectors.o: \
- /Users/mafaneh/Ebook\ Project/nRF5_SDK_14.2.0_17b948a/components/toolchain/ses/ses_nrf52840_Vectors.s
